f4e079ffe479ffa3cddd2475effd8902aa0caa45
Sylvain <legrills@gmail.com>
2019-08-27 15:52:18 +0200

    more logics

--- staff/sylvain-legrill/my-stuff-api/logic/card/register/index.spec.js
+++ staff/sylvain-legrill/my-stuff-api/logic/card/register/index.spec.js
@@ -1,55 +1,88 @@
-const { MongoClient } = require('mongodb')
+const mongoose = require('mongoose')
+const logic = require('../../.')
 const { expect } = require('chai')
-const logic = require('../../')
+const { User, Card } = require('../../../models')
 
-describe('logic -', () => {
-    let client, users
+describe.only('logic - register card', () => {
 
-    before(() => {
+    before(() => mongoose.connect('mongodb://localhost/my-api-test', { useNewUrlParser: true }))
+ 
+    let name,surname,email,password
+    let id,number,expiry, cardId    
+ 
+    beforeEach(() => {
+              
+                number = `1-${Math.random()}`
+                expiry = `0${Math.floor(Math.random() * 9)}/0${Math.floor(Math.random() * 9)}`
+                  
 
-       client = new MongoClient('mongodb://localhost', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }) 
-
-        return client.connect()
+        return Card.deleteMany()
+                     // Register user first to make sure it exists
             .then(() => {
-                const db = client.db('skylab')
+                name = `name-${Math.random()}`
+                surname = `surname-${Math.random()}`
+                email = `email-${Math.random()}@email.com`
+                password = `123-${Math.random()}`
 
-                users = db.collection('users')
-
-                logic.__users__ = users
+                return User.create({ name, surname, email, password })
             })
+            .then(user => id = user.id.toString()) 
+                        
     })
 
-    beforeEach(() => users.deleteMany())
-
-    describe('register', () => {
-        let name, surname, email, password, repassword
-
-        beforeEach(() => {
-            name = `name-${Math.random()}`
-            surname = `surname-${Math.random()}`
-            email = `email-${Math.random()}@domain.com`
-            password = `password-${Math.random()}`
-        })
-
-        it('should succeed on correct data', () =>
-        logic.registerUser(number, expiry)
-            .then(result => {
-                expect(result).not.to.exist
-
-                return Card.findOne({ number })
-            })
-            .then(card => {
+    it('should succeed on correct data', () =>{        
+        logic.registerCard(id,cardId)
+        
+            .then(card => { 
+                  
                 expect(card).to.exist
-                expect(card.number).to.equal(number)
-                expect(card.number).to.be(number.length)
+                expect(card.number).to.deep.equal(number)
                 expect(card.expiry).to.equal(expiry)
-                expect(card.password).to.equal(password)
-            })
+            })}
+    
+        
     )
+
+
+    it('should fail if there is no user', () => {
+            logic.registerCard("5e624a1e0e56cb055f56d3d0", cardId)
+               .catch( error =>{
+                debugger
+                   expect(error).to.exist
+                   expect(error.message).to.equal(`User does not exists.`)
+               })
+           
+            })
+
+
+    it('should fail if there is no card', () =>{
+        let x= '5d5d5530531d455f75da9fF9'
+        logic.registerCard(id, x)
+               .catch( error =>{
+                   expect(error).to.exist
+                   expect(error.message).to.equal(`Card with id ${x} does not exist.`)
+               })
+    }) 
+
+    it('should fail on empty number', () => {
+        expect(() =>
+            logic.registerCard("", number)
+        ).to.throw('id is empty or blank')
     })
 
-    after(() => client.close())
+    it('should fail on undefined number', () => {
+        expect(() =>
+            logic.registerCard(undefined, number)
+        ).to.throw(`id with value undefined is not a string`)
+    })
+
+    it('should fail on wrong data type', () => {
+        expect(() =>
+            logic.registerCard(123, number )
+        ).to.throw(`id with value 123 is not a string`)
+    })
+
+
+
+    after(() => mongoose.disconnect())
 })
