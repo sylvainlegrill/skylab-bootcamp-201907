8c7c91da07d87dcfc59ff8b20b818d06da8b7470
Sylvain <legrills@gmail.com>
2019-09-06 10:05:58 +0200

    update user logic

--- staff/sylvain-legrill/jamba/jamba-api/logic/user/register-user-architect/index.js
+++ staff/sylvain-legrill/jamba/jamba-api/logic/user/register-user-architect/index.js
@@ -7,19 +7,21 @@ const { models: { User } } = require('jamba-data')
  * @param {string} name 
  * @param {string} surname 
  * @param {string} email 
- * @param {number} number
- * @param {license} string
+ * @param {string} phone
+ * @param {string} city
+ * @param {string} license
+ * @param {string} specialty
  * @param {string} password
  * 
  * @returns {Promise}
  */
-module.exports = function (name, surname, email, phone, license, password) {
+module.exports = function (name, surname, email, phone, city, license, specialty, password) {
 
     return User.findOne({ email })
         .then(user => {
             if (user) throw new Error(`user with e-mail ${email} already exists`)
 
-            return User.create({ name, surname, email, phone, license, password })
+            return User.create({ name, surname, email, phone, city, license, specialty, password })
         })
         .then(() => { })
 }
--- staff/sylvain-legrill/jamba/jamba-api/logic/user/register-user-architect/index.spec.js
+++ staff/sylvain-legrill/jamba/jamba-api/logic/user/register-user-architect/index.spec.js
@@ -9,21 +9,23 @@ const { env: { DB_URL_TEST }} = process
 describe('logic - register user', () => {
     before(() => database.connect(DB_URL_TEST))
 
-    let name, surname, email, phone, license, password
+    let name, surname, email, phone, city, license, specialty, password
 
     beforeEach(() => {
         name = `name-${Math.random()}`
         surname = `surname-${Math.random()}`
         email = `email-${Math.random()}@domain.com`
-        phone = number(600000000, 799999999)
+        phone = `phone-${Math.random()}`
+        city = `city-${Math.random()}`
         license = `license-${Math.random()}`
+        specialty = `specialty-${Math.random()}`
         password = `password-${Math.random()}`
 
         return User.deleteMany()
     })
 
     it('should succeed on correct data', () =>
-    registerArchitect(name, surname, email, phone, license, password)
+    registerArchitect(name, surname, email, phone, city, license, specialty, password)
             .then(result => {
                 expect(result).not.to.exist
 
@@ -35,7 +37,9 @@ describe('logic - register user', () => {
                 expect(user.surname).to.equal(surname)
                 expect(user.email).to.equal(email)
                 expect(user.phone).to.equal(phone)
+                expect(user.city).to.equal(city)
                 expect(user.license).to.equal(license)
+                expect(user.specialty).to.equal(specialty)
                 expect(user.password).to.equal(password)
             })
     )
